c79a2f03ef26a0faf8d4fb9a327d7803fe68fe8d
nascimentofernanda <nascimentofernandadev@gmail.com>
2023-03-11 13:06:07 -0300

    Tópico "conhecendo a linguagem Java" finalizado

---
+++ Java/src/aulas/básico/exercícios/Métodos.java
@@ -0,0 +1,11 @@
+package aulas.básico.exercícios;
+
+public class Métodos {
+    //deve ser nomeado como verbo
+    //segue o padrão camelCase
+    //métodos tem apenas uma responsabilidade
+    //todos os métodos devem ser definidos dentro de uma classe
+    //caso não tenha retorno o método é representado pela palavra VOID
+    //caso tenha retorno é preciso terminar o método com o RETURN
+    
+}
